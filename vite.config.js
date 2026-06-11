1dbdd33a5a40645e1e69569560e5c8e322e680c0
enereloyu <heenreel@gmail.com>
2026-06-11 14:43:23 +0800

    zaswar

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  base: command === 'build' ? '/video-label/' : '/',
+  build: { outDir: 'docs' },
   server: { host: true, port: 5173 },
-})
+}))
